5f1bd494d03dc1be9621800c391d8a98da34e74d
Holandara <saraholandatavares@gmail.com>
2025-02-17 15:02:59 -0300

    testando

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import HeaderSection from './components/HeaderSection/HeaderSection.js';
 import Divider from './components/Divider/Divider.js';
 import LearnSection from './components/LearnSection/LearnSection.js';
 import InvestSection from './components/InvestSection/InvestSection.js';
-import NavBar from './components/NavBar/NavBar.js';
 
 function App() {
   return (
