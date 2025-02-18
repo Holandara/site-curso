499590d20dfa06a50df7c221e4b8db62523ae582
Holandara <saraholandatavares@gmail.comm>
2025-02-18 08:43:37 -0300

    organizar

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
       <LearnSection />
       <Divider />
       <InvestSection />
+      
     </div>
   );
 }
--- src/components/HeaderSection/HeaderSection.js
+++ src/components/HeaderSection/HeaderSection.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './HeaderSection.css';
+import { motion } from "framer-motion"; // Importação correta do framer-motion
+import { scroll } from "motion";
 
 const HeaderSection = () => {
   return (
@@ -19,18 +21,32 @@ const HeaderSection = () => {
 
           {/* Coluna direita | com imagens */}
           <div className="lado2 col-start-2 row-span-full relative responsive">
-            <div className="blob-image absolute">
-              <img src="blob.svg" alt="blob" />
-            </div>
-            <div className="blob-line absolute">
-              <img src="blob-line.svg" alt="blob line" />
-            </div>
-            <div className="imagem1 absolute">
-              <img className="imagemBraco" src="foto-braco.jpg" alt="img braco" />
-            </div>
-            <div className="imagem2 absolute">
-              <img className="imagemMascara" src="Fitchek_Baixa-1.jpg" alt="img fitcheck" />
-            </div>
+    
+              <div className="blob-image absolute">
+                <img src="blob.svg" alt="blob" />
+              </div>
+              <div className="blob-line absolute">
+                <img src="blob-line.svg" alt="blob line" />
+              </div>
+              <motion.div
+              initial={{ opacity: 0 }} // Começa invisível
+              animate={{ opacity: 1 }} // Termina totalmente visível
+              transition={{ duration: 1.5 }} // Duração da animação (1 segundo)
+            >
+              <div className="imagem1 absolute">
+                <img className="imagemBraco" src="foto-braco.jpg" alt="img braco" />
+              </div>
+              </motion.div>
+              <motion.div
+              initial={{ opacity: 0 }} // Começa invisível
+              animate={{ opacity: 1 }} // Termina totalmente visível
+              transition={{ duration: 1}} // Duração da animação (1 segundo)
+            >
+              <div className="imagem2 absolute">
+                <img className="imagemMascara" src="Fitchek_Baixa-1.jpg" alt="img fitcheck" />
+              </div>
+              </motion.div>
+            
           </div>
         </div>
       </div>
