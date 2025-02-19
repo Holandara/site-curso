c27570aed0569b807160e2f89b84d055643daa93
Theo Penha Valente <theopvalente@gmail.com>
2025-02-19 16:11:29 -0300

    modal-v2

--- src/components/Adicional/Modal/Modal.jsx
+++ src/components/Adicional/Modal/Modal.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
-import './Modal.css';
+import React from "react";
+import "./Modal.css";
 
-export default function Modal({isOpen}) {
+export default function Modal({ isOpen, setIsOpen }) {
+  const handleCloseModal = () => {
+    setIsOpen(false);
+  };
 
-  if (isOpen) {
-    return <div>Modal</div>
-  }
+  if (!isOpen) return null;
 
-  return null
+  return (
+    <div className="modal-overlay">
+      <div className="modal">
+        <div className="modal-header">
+          <h2>Informações do Curso</h2>
+          <button className="modal-close" onClick={handleCloseModal}>
+            &times;
+          </button>
+        </div>
+        <div className="modal-body">
+          <p>Preencha o formulário abaixo para reservar sua vaga:</p>
+          {/* Adicione um formulário aqui */}
+        </div>
+        <div className="modal-footer">
+          <button className="modal-button" onClick={handleCloseModal}>
+            Fechar
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/Section3/InvestSection.js
+++ src/components/Section3/InvestSection.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import CourseBlock from './CourseBlock/CourseBlock';
-import './InvestSection.css';
+import React from "react";
+import CourseBlock from "./CourseBlock/CourseBlock";
+import "./InvestSection.css";
 
-const InvestSection = () => {
+const InvestSection = ({ setOpen, setSelected }) => {
   return (
     <div>
-      <div className="carreiraTexto" id='contato'>
-        invista <span className="carreiraTexto2" >hoje</span> na sua{' '}
+      <div className="carreiraTexto" id="contato">
+        invista <span className="carreiraTexto2">hoje</span> na sua{" "}
         <span className="carreiraTexto2">carreira!</span>
       </div>
       <div className="flex md:flex-row-reverse flex-wrap">
@@ -16,6 +16,8 @@ const InvestSection = () => {
           time="7h às 17h"
           price="250"
           discount="Desconto de **% para profissionais Cref7"
+          setOpen={setOpen}
+          setSelected={setSelected}
         />
         <CourseBlock
           title="Dia 05/05 e 06/05"
@@ -23,6 +25,8 @@ const InvestSection = () => {
           time="7h às 17h"
           price="250"
           discount="Desconto de **% para profissionais Cref7"
+          setOpen={setOpen}
+          setSelected={setSelected}
         />
       </div>
     </div>
